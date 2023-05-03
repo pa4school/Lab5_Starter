c6991512352379b31974dbd3909b02ef7b26b1d0
Anh Pham <phuanh004@gmail.com>
2023-05-02 22:33:03 -0700

    Add image select

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,41 @@
 // expose.js
 
-window.addEventListener('DOMContentLoaded', init);
+/**
+ * Change the horn image based on the horn selected
+ * @param {HTMLSelectElement} select - The horn select element
+ * @param {HTMLImageElement} img - The horn image element
+ */
+const changeHorn = (select, img) => {
+	const horn = select.value;
 
-function init() {
-  // TODO
+	switch (horn) {
+		case 'air-horn':
+			img.src = 'assets/images/air-horn.svg';
+			img.alt = 'Air Horn';
+			break;
+		case 'car-horn':
+			img.src = 'assets/images/car-horn.svg';
+			img.alt = 'Car Horn';
+			break;
+		case 'party-horn':
+			img.src = 'assets/images/party-horn.svg';
+			img.alt = 'Party Horn';
+			break;
+		default:
+			img.src = 'assets/images/no-image.png';
+			img.alt = 'No Image';
+			break;
+	}
 }
+
+/**
+ * Initialize the expose page
+ */
+const init = () => {
+	const hornSelect = document.getElementById('horn-select');
+	const hornImg = document.querySelector("#expose>img");
+
+	hornSelect.addEventListener('change', changeHorn.bind(null, hornSelect, hornImg));
+}
+
+window.addEventListener('DOMContentLoaded', init);
