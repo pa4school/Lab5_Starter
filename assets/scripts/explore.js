23d3a697901eb21a8a88fe9d7f15c2586bbfef41
Nicholas Yousefi <nsyousef@ucsd.edu>
2023-05-03 19:43:40 -0700

    Add mouth opening functionality

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,13 +1,14 @@
 // explore.js
 
-window.addEventListener('DOMContentLoaded', init);
-
+// create variables to access elements of HTML document
 const voiceMenu = document.getElementById("voice-select");
 const talkButton = document.querySelector("button");
 const textBox = document.getElementById("text-to-speak");
-const faceImage = document.querySelector("image");
+const faceImage = document.querySelector("img");
 
 let voices = [];
+
+window.addEventListener('DOMContentLoaded', init);
 
 /**
  * Populates the dropdown menu with the list of possible voices.
@@ -23,16 +24,33 @@ function addVoicesToMenu() {
   }
 }
 
+/**
+ * Initializes the web page after it finishes loading.
+ */
 function init() {
+  // populate voice menu
   addVoicesToMenu();
   if (speechSynthesis.onvoiceschanged !== undefined) {
     speechSynthesis.onvoiceschanged = addVoicesToMenu;
   }
 
+  // set button up to speak text when clicked
   talkButton.addEventListener('click', function() {
     let textToSpeak = textBox.value;
     const utterance = new SpeechSynthesisUtterance(textToSpeak);
     utterance.voice = voices[voiceMenu.value];
+    utterance.addEventListener('start', function() {
+      faceImage.src = "assets/images/smiling-open.png";
+      voiceMenu.disabled = true;
+      talkButton.disabled = true;
+      textBox.disabled = true;
+    });
+    utterance.addEventListener('end', function() {
+      faceImage.src = "assets/images/smiling.png";
+      voiceMenu.disabled = false;
+      talkButton.disabled = false;
+      textBox.disabled = false;
+    });
     window.speechSynthesis.speak(utterance);
   });
 }
