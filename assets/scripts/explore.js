303161c7ccb7a2c0a92fbe3ac130459f5970ceca
Nicholas Yousefi <nsyousef@ucsd.edu>
2023-05-03 18:37:09 -0700

    Add (incomplete) speech functionality

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,6 +2,37 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+const voiceMenu = document.getElementById("voice-select");
+const talkButton = document.querySelector("button");
+const textBox = document.getElementById("text-to-speak");
+const faceImage = document.querySelector("image");
+
+let voices = [];
+
+/**
+ * Populates the dropdown menu with the list of possible voices.
+ */
+function addVoicesToMenu() {
+  voices = window.speechSynthesis.getVoices();
+
+  for (let i = 0; i < voices.length; i++) {
+    const voiceOption = document.createElement("option");
+    voiceOption.textContent = voices[i].name + " (" + voices[i].lang + ")";
+    voiceOption.value = i;
+    voiceMenu.appendChild(voiceOption);
+  }
+}
+
 function init() {
-  // TODO
+  addVoicesToMenu();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = addVoicesToMenu;
+  }
+
+  talkButton.addEventListener('click', function() {
+    let textToSpeak = textBox.value;
+    const utterance = new SpeechSynthesisUtterance(textToSpeak);
+    utterance.voice = voices[voiceMenu.value];
+    window.speechSynthesis.speak(utterance);
+  });
 }
